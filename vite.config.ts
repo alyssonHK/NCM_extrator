a64d51b5c969dd45368744278f41fe46f89b0d55
Ambiental Limpeza Urbana e Saneamento LTDA <alyssonhk@gmail.com>
2025-06-28 16:26:31 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      server: {
+        port: 5173,
+        strictPort: true
       }
     };
 });
